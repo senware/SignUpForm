3aa135c4d74e055ee1ee4a6c834e3af87c914bd1
Sen <tug81999@temple.edu>
2021-02-17 00:04:19 -0500

    Added message on successful form completion

--- app/src/main/java/edu/temple/MainActivity.java
+++ app/src/main/java/edu/temple/MainActivity.java
@@ -3,10 +3,12 @@ package edu.temple;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.Gravity;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.TextView;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -18,39 +20,61 @@ public class MainActivity extends AppCompatActivity {
         submitButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                String firstName = ((EditText) findViewById(R.id.firstNameEditText)).getText().toString();
-                String lastName = ((EditText) findViewById(R.id.lastNameEditText)).getText().toString();
-                String email = ((EditText) findViewById(R.id.emailEditText)).getText().toString();
-                String password = ((EditText) findViewById(R.id.passwordEditText)).getText().toString();
-                String passwordConfirm = ((EditText) findViewById(R.id.passwordConfirmEditText)).getText().toString();
-
+                // pointer to hold error message TextViews
                 TextView emptyFieldError;
 
+                // point to the first error message and hide it
                 emptyFieldError = findViewById(R.id.firstNameErrorTextView);
                 emptyFieldError.setText("");
+
+                // point to the contents of the first name field
+                String firstName = ((EditText) findViewById(R.id.firstNameEditText)).getText().toString();
+
+                // check to see if the first name field is empty
                 if (firstName.matches("")) {
                     emptyFieldError.setText(getString(R.string.firstNameErrorMsg));
                     return;
                 }
 
+                // point to the next error message and hide it
                 emptyFieldError = findViewById(R.id.lastNameErrorTextView);
                 emptyFieldError.setText("");
+
+                // point to the contents of the last name field
+                String lastName = ((EditText) findViewById(R.id.lastNameEditText)).getText().toString();
+
+                // check to see if the last name field is empty
                 if(lastName.matches("")) {
                     emptyFieldError.setText(getString(R.string.lastNameErrorMsg));
                     return;
                 }
 
+                // point to the next error message and hide it
                 emptyFieldError = findViewById(R.id.emailErrorTextView);
                 emptyFieldError.setText("");
+
+                // point to the contents of the email field
+                String email = ((EditText) findViewById(R.id.emailEditText)).getText().toString();
+
+                // check to see if the email field is empty
                 if(email.matches("")) {
                     emptyFieldError.setText(getString(R.string.emailErrorMsg));
                     return;
                 }
 
+                // hide both errors for password and password confirm fields
                 emptyFieldError = findViewById(R.id.passwordErrorTextView);
                 emptyFieldError.setText("");
                 emptyFieldError = findViewById((R.id.passwordConfirmErrorTextView));
                 emptyFieldError.setText("");
+
+                // point to the password and password confirm field's contents
+                String password = ((EditText) findViewById(R.id.passwordEditText)).getText().toString();
+                String passwordConfirm = ((EditText) findViewById(R.id.passwordConfirmEditText)).getText().toString();
+
+                // check to see if the password field is blank
+                // or check to see if the password and password confirm fields don't match
+                // conditions must me in this order, otherwise short-circuiting would allow both fields to be empty
                 if (password.matches("") || !password.matches(passwordConfirm)) {
                     emptyFieldError = findViewById(R.id.passwordErrorTextView);
                     emptyFieldError.setText(getString(R.string.passwordErrorMsg));
@@ -58,6 +82,13 @@ public class MainActivity extends AppCompatActivity {
                     emptyFieldError.setText(getString(R.string.passwordErrorMsg));
                     return;
                 }
+
+                // if no errors occur, display a little welcome message
+                Toast welcome;
+                // yeah this could have been easier to read, but I kind of like silly looking function calls
+                welcome = Toast.makeText(getApplicationContext(), "Welcome, " + firstName + " " + lastName + "!", Toast.LENGTH_LONG);
+                // display message
+                welcome.show();
             }
         });
     }
